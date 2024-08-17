c769897ab14030998dfc4d81d79753151bc7d56f
IMFinallyHere <priyanshkhandelwal5@gmail.com>
2024-08-18 04:25:33 +0530

    register models for admin site. Added requirements.txt

--- management/admin.py
+++ management/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(CounterCodes)
+admin.site.register(Customers)
+admin.site.register(Group)
+admin.site.register(CustomerGroup)
+admin.site.register(RoomType)
+admin.site.register(Rooms)
+admin.site.register(RoomsPriceChart)
+admin.site.register(RoomStayLogs)
+admin.site.register(Configurations)
